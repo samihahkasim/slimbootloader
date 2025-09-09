bb37cbfed1e9adafb648e73b68e59886e5ad3c0c
skasim <samihah.kasim@intel.com>
2025-09-09 14:40:18 +0800

    feat: [RPLSR] Update for RPLSR MR5
    - FSP: RPL-S Refresh 6074_00 (0C.00.F9.20)
    - Microcode: m_32_b0671_00000130
    
    Signed-off-by: samihahkasim <samihah.kasim@intel.com>

--- Platform/RaptorlakeBoardPkg/BoardConfigRpls.py
+++ Platform/RaptorlakeBoardPkg/BoardConfigRpls.py
@@ -22,9 +22,9 @@ class Board(BaseBoard):
 
         super(Board, self).__init__(*args, **kwargs)
 
-        self.VERINFO_IMAGE_ID     = 'SB_RPLS'
+        self.VERINFO_IMAGE_ID     = 'SB_RPLSR'
         self.VERINFO_PROJ_MAJOR_VER = 1
-        self.VERINFO_PROJ_MINOR_VER = 6
+        self.VERINFO_PROJ_MINOR_VER = 5
         self.VERINFO_SVN            = 1
         self.VERINFO_BUILD_DATE     = time.strftime("%m/%d/%Y")
 
